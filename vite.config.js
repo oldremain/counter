c4cdbe9f1288676f406a3f3d73823428ae37e956
Олег Зайцев <30oleg.zv94@gmail.com>
2025-11-08 11:01:03 +0300

    init: start project

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "dashesOnly", // или 'camelCaseOnly', 'dashes', 'camelCase'
+      generateScopedName: "[name]__[local]___[hash:base64:5]", // формат: ComponentName_className_hash
+    },
+  },
+});
